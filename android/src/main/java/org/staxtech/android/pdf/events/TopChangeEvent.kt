a0ddcf450fada190cc11eeb49a7b27fdfb2fe41e
DanfordGidraph <danfordmaina@gmail.com>
2024-07-04 14:18:06 +0300

    * refactor(TopChangeEvent.kt): * - Remove original author comment and update Staxtech copyright information * - Change package name to org.staxtech.android.pdf.events * - Import necessary dependencies * - Change class visibility to internal * - Update constructor parameter names * - Override getEventName() method to return "topChange" * - Override getEventData() method to return eventData

--- android/src/main/java/org/staxtech/android/pdf/events/TopChangeEvent.kt
+++ android/src/main/java/org/staxtech/android/pdf/events/TopChangeEvent.kt
@@ -1,35 +1,22 @@
 /**
- * Original Author: Wonday (@wonday.org) Copyright (c) 2017-present
  * Copyright (c) 2024-present, Staxtech (@gidraphdanford.dev)
  * All rights reserved.
  *
  * This source code is licensed under the MIT-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+package org.staxtech.android.pdf.events
 
-package org.staxtech.android.pdf.events;
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.uimanager.events.Event
 
-import androidx.annotation.Nullable;
-
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.uimanager.events.Event;
-
-public class TopChangeEvent extends Event<TopChangeEvent> {
-    private final WritableMap eventData;
-
-    public TopChangeEvent(int surfaceId, int viewTag, WritableMap data) {
-        super(surfaceId, viewTag);
-        eventData = data;
+internal class TopChangeEvent(surfaceId: Int, viewTag: Int, private val eventData: WritableMap) :
+    Event<TopChangeEvent>(surfaceId, viewTag) {
+    override fun getEventName(): String {
+        return "topChange"
     }
 
-    @Override
-    public String getEventName() {
-        return "topChange";
-    }
-
-    @Nullable
-    @Override
-    protected WritableMap getEventData() {
-        return eventData;
+    override fun getEventData(): WritableMap {
+        return eventData
     }
 }
